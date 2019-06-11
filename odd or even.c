4a8b5cf962c50bf783794bfc283312d6090afc9c
purushottam1997 <45572099+purushottam1997@users.noreply.github.com>
2019-06-11 15:08:24 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("Even");
+    }
+    else if(n%2!=0)
+    {
+    printf("Odd");}
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
